4ef7e7f73dd4e16bd17534417cc21dafd992fbea
yoouung <jyp101400@egmail.com>
2024-06-17 16:59:57 +0900

    feat: add POST db connection
    temp

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -6,16 +6,11 @@ const PostController = require("../controllers/postController.js");
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 
-/* posts listing. */
 router.get("/", PostController.getPosts);
 router.get("/:post_id", PostController.getPostById);
 
 router.post("/new", upload.single("image"), PostController.createPost);
 router.patch("/:post_id", upload.single("image"), PostController.updatePost);
 router.delete("/:post_id", PostController.deletePost);
-
-router.post("/:post_id/comment", PostController.createComment);
-router.patch("/:post_id/comment/:comment_id", PostController.updateComment);
-router.delete("/:post_id/comment/:comment_id", PostController.deleteComment);
 
 module.exports = router;
