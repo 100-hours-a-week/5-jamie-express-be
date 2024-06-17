0f5a40391854810465d2f42f7338bd136030dbf1
yoouung <jyp101400@egmail.com>
2024-06-17 16:59:57 +0900

    feat: add POST db connection

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,5 @@
 const Post = require("../model/postModel.js");
 
-// ===== POSTS ====
-
 const getPosts = (req, res) => {
     if (!req.session.user) {
         res.status(401).json({ message: "로그인 정보 없음" });
@@ -12,11 +10,16 @@ const getPosts = (req, res) => {
     const limit = parseInt(req.query.limit) || 5;
 
     try {
-        const { posts, total, currentPage } = Post.getPosts(page, limit);
-        res.status(200).json({
-            posts: posts,
-            total: total,
-            currentPage: currentPage,
+        Post.getPosts(page, limit, (result) => {
+            const { status, posts } = result;
+
+            if (status === 401) {
+                res.status(401).json({ message: "게시글 정보 없음" });
+            } else if (status === 200) {
+                res.status(200).json({
+                    posts: posts,
+                });
+            }
         });
     } catch (error) {
         console.error("게시글 불러오기 에러: ", error);
@@ -33,14 +36,18 @@ const getPostById = (req, res) => {
     const { post_id } = req.params;
 
     try {
-        const { status, post } = Post.getPostById(post_id);
+        Post.getPostById(post_id, (result) => {
+            const { status, post } = result;
 
-        if (status === 404) {
-            res.status(404).json({ message: "게시글 정보 없음" });
-        } else if (status === 200) {
-            const current_user_id = req.session.user.user_id;
-            res.status(200).json({ post: post, current_user_id: current_user_id });
-        }
+            if (status === 404) {
+                res.status(404).json({ message: "게시글 정보 없음" });
+            } else if (status === 401) {
+                res.status(401).json({ message: "조회수 업데이트 실패" });
+            } else if (status === 200) {
+                const current_user_id = req.session.user.user_id;
+                res.status(200).json({ post: post, current_user_id: current_user_id });
+            }
+        });
     } catch (error) {
         console.error("게시글 불러오기 에러: ", error);
         res.status(500).send("Internal Server Error");
@@ -58,18 +65,23 @@ const createPost = (req, res) => {
     const image = req.file;
 
     try {
-        const { status, post_id } = Post.createPost({
-            user_id,
-            title,
-            content,
-            image,
-        });
+        Post.createPost(
+            {
+                user_id,
+                title,
+                content,
+                image,
+            },
+            (result) => {
+                const { status, post_id } = result;
 
-        if (status === 400) {
-            res.status(400).json({ message: "누락된 정보가 있습니다." });
-        } else if (status === 200) {
-            res.status(200).json({ post_id: post_id });
-        }
+                if (status === 400) {
+                    res.status(400).json({ message: "누락된 정보가 있습니다." });
+                } else if (status === 200) {
+                    res.status(200).json({ post_id: post_id });
+                }
+            }
+        );
     } catch (error) {
         console.error("게시글 작성 에러: ", error);
         res.status(500).send("Internal Server Error");
@@ -88,21 +100,26 @@ const updatePost = (req, res) => {
     const image = req.file;
 
     try {
-        const { status, post_updated_id } = Post.updatePost({
-            user_id,
-            post_id,
-            title,
-            content,
-            image,
-        });
+        Post.updatePost(
+            {
+                user_id,
+                post_id,
+                title,
+                content,
+                image,
+            },
+            (result) => {
+                const { status, post_updated_id } = result;
 
-        if (status === 404) {
-            res.status(404).json({ message: "게시글 정보 없음" });
-        } else if (status === 409) {
-            res.status(409).json({ message: "게시글 수정 권한 없음" });
-        } else if (status === 200) {
-            res.status(200).json({ post_id: post_updated_id });
-        }
+                if (status === 404) {
+                    res.status(404).json({ message: "게시글 정보 없음" });
+                } else if (status === 409) {
+                    res.status(409).json({ message: "게시글 수정 권한 없음" });
+                } else if (status === 200) {
+                    res.status(200).json({ post_id: post_updated_id });
+                }
+            }
+        );
     } catch (error) {
         console.error("게시글 수정 에러: ", error);
         res.status(500).send("Internal Server Error");
@@ -119,110 +136,19 @@ const deletePost = async (req, res) => {
     const { post_id } = req.params;
 
     try {
-        const { status, post_deleted_id } = Post.deletePost({ user_id, post_id });
+        Post.deletePost({ user_id, post_id }, (result) => {
+            const { status, post_deleted_id } = result;
 
-        if (status === 404) {
-            res.status(404).json({ message: "게시글 정보 없음" });
-        } else if (status === 409) {
-            res.status(409).json({ message: "게시글 삭제 권한 없음" });
-        } else if (status === 200) {
-            res.status(200).json({ post_id: post_deleted_id });
-        }
+            if (status === 404) {
+                res.status(404).json({ message: "게시글 정보 없음" });
+            } else if (status === 409) {
+                res.status(409).json({ message: "게시글 삭제 권한 없음" });
+            } else if (status === 200) {
+                res.status(200).json({ post_id: post_deleted_id });
+            }
+        });
     } catch (error) {
         console.error("게시글 삭제 에러: ", error);
-        res.status(500).send("Internal Server Error");
-    }
-};
-
-// ===== COMMENTS ====
-
-const createComment = (req, res) => {
-    if (!req.session.user) {
-        res.status(401).json({ message: "로그인 정보 없음" });
-        return;
-    }
-
-    const { user_id } = req.session.user;
-    const comment = req.body;
-    const { post_id } = req.params;
-
-    try {
-        const { status, comment_id } = Post.createComment({
-            post_id,
-            comment,
-            user_id,
-        });
-        if (status === 400) {
-            res.status(400).json({ message: "게시글 정보 없음" });
-        } else if (status === 200) {
-            res.status(200).json({ comment_id: comment_id });
-        }
-    } catch (error) {
-        console.error("댓글 작성 에러: ", error);
-        res.status(500).send("Internal Server Error");
-    }
-};
-
-const updateComment = (req, res) => {
-    if (!req.session.user) {
-        res.status(401).json({ message: "로그인 정보 없음" });
-        return;
-    }
-
-    const { user_id } = req.session.user;
-    const { post_id, comment_id } = req.params;
-    const update_form = req.body;
-
-    try {
-        const { status, comment_updated_id } = Post.updateComment({
-            user_id,
-            post_id,
-            comment_id,
-            update_form,
-        });
-
-        if (status === 400) {
-            res.status(400).json({ message: "게시글 정보 없음" });
-        } else if (status === 404) {
-            res.status(404).json({ message: "댓글 정보 없음" });
-        } else if (status === 409) {
-            res.status(409).json({ message: "댓글 수정 권한 없음" });
-        } else if (status === 200) {
-            res.status(200).json({ comment_id: comment_updated_id });
-        }
-    } catch (error) {
-        console.error("댓글 수정 에러: ", error);
-        res.status(500).send("Internal Server Error");
-    }
-};
-
-const deleteComment = async (req, res) => {
-    if (!req.session.user) {
-        res.status(401).json({ message: "로그인 정보 없음" });
-        return;
-    }
-
-    const { user_id } = req.session.user;
-    const { post_id, comment_id } = req.params;
-
-    try {
-        const { status, comment_deleted_id } = Post.deleteComment({
-            user_id,
-            post_id,
-            comment_id,
-        });
-
-        if (status === 400) {
-            res.status(400).json({ message: "게시글 정보 없음" });
-        } else if (status === 404) {
-            res.status(404).json({ message: "댓글 정보 없음" });
-        } else if (status === 409) {
-            res.status(409).json({ message: "댓글 삭제 권한 없음" });
-        } else if (status === 200) {
-            res.status(200).json({ comment_id: comment_deleted_id });
-        }
-    } catch (error) {
-        console.error("댓글 삭제 에러: ", error);
         res.status(500).send("Internal Server Error");
     }
 };
@@ -233,7 +159,4 @@ module.exports = {
     createPost,
     updatePost,
     deletePost,
-    createComment,
-    updateComment,
-    deleteComment,
 };
--- model/postModel.js
+++ model/postModel.js
@@ -1,268 +1,169 @@
-const fs = require("fs");
-const path = require("path");
-const getKoreanDateTime = require("../utils/dateFormat.js");
+const db = require("../config/mysql.js");
+const conn = db.init();
 
-let postsJSON;
+const getPosts = (page, limit, callback) => {
+    console.log("============ [POST] GET posts");
 
-// ===== POSTS =====
-
-const getPosts = (page, limit) => {
-    postsJSON = JSON.parse(
-        fs.readFileSync(path.join(__dirname, "../data", "posts.json"), "utf-8")
-    );
-    const usersJSON = JSON.parse(
-        fs.readFileSync(path.join(__dirname, "../data", "users.json"), "utf-8")
-    );
-
-    const postList = postsJSON.map((post) => {
-        const { title, likes, comments, hits, created_at, post_id, user_id } = post;
-        const nickname = usersJSON.find((user) => user.user_id === user_id).nickname;
-        const profile_image = usersJSON.find(
-            (user) => user.user_id === post.user_id
-        ).profile_image;
-
-        return {
-            title,
-            likes,
-            comments,
-            hits,
-            created_at,
-            post_id,
-            nickname,
-            profile_image,
-        };
-    });
-
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
-    const paginatedPosts = postList.slice(startIndex, endIndex);
-
-    console.log(`[POST] GET posts from ${startIndex} to ${endIndex}`);
-    return { posts: paginatedPosts, total: postList.length, currentPage: page };
-};
-
-const getPostById = (post_id) => {
-    postsJSON = JSON.parse(
-        fs.readFileSync(path.join(__dirname, "../data", "posts.json"), "utf-8")
-    );
-    const usersJSON = JSON.parse(
-        fs.readFileSync(path.join(__dirname, "../data", "users.json"), "utf-8")
-    );
-
-    // 게시글 정보, 작성자 닉네임, 작성자 프로필 이미지 결합하여 반환
-    const post = postsJSON.find((post) => post.post_id === parseInt(post_id));
-
-    if (!post) {
-        return { status: 404 };
+    if (!conn) {
+        console.error("[POST] GET posts: DB connection error");
+        return callback({ status: 500 });
     }
 
-    const nickname = usersJSON.find((user) => user.user_id === post.user_id).nickname;
-    const profile_image = usersJSON.find(
-        (user) => user.user_id === post.user_id
-    ).profile_image;
-    const commentsList = post.comments_list.map((comment) => {
-        const user = usersJSON.find((user) => user.user_id === comment.user_id);
-        return {
-            ...comment,
-            nickname: user.nickname,
-            profile_image: user.profile_image,
-        };
+    const sql = `SELECT Posts.*, Users.nickname, Users.profileImage
+                FROM Posts
+                LEFT JOIN Users ON Posts.userId = Users.id
+                ORDER BY Posts.id DESC
+                `;
+
+    conn.query(sql, function (err, result) {
+        if (err) {
+            console.error("[POST] GET posts: ", err);
+            return callback({ status: 401 });
+        } else {
+            const startIndex = (page - 1) * limit;
+            const endIndex = page * limit;
+            const paginatedPosts = result.slice(startIndex, endIndex);
+
+            console.log(`[POST] GET posts from ${startIndex} to ${endIndex}`);
+            return callback({
+                status: 200,
+                posts: paginatedPosts,
+            });
+        }
     });
-
-    const postWithAuthor = {
-        ...post,
-        nickname,
-        profile_image,
-        comments_list: commentsList,
-    };
-
-    post.hits += 1;
-    savePosts();
-
-    console.log("[POST] GET post by id: ", post.post_id);
-    return { status: 200, post: postWithAuthor };
 };
 
-const createPost = ({ user_id, title, content, image }) => {
-    postsJSON = JSON.parse(
-        fs.readFileSync(path.join(__dirname, "../data", "posts.json"), "utf-8")
-    );
+const getPostById = (post_id, callback) => {
+    console.log("============ [POST] GET post by id");
+
+    if (!conn) {
+        console.error("[POST] GET post by id: DB connection error");
+        return { status: 500 };
+    }
+
+    const sql = `SELECT Posts.*,
+                        Users.nickname, 
+                        Users.profileImage, 
+                        Comments.id AS commentId, 
+                        Comments.userId AS commentUserId, 
+                        Comments.content AS commentContent, 
+                        Comments.createdAt AS commentCreatedAt,
+                        Comments.updatedAt AS commentUpdatedAt
+                FROM Posts 
+                JOIN Users ON Posts.userId = Users.id 
+                LEFT JOIN Comments ON Posts.id = Comments.postId 
+                WHERE Posts.id = ?`;
+
+    let post;
+    conn.query(sql, [post_id], function (err, result) {
+        if (err) {
+            console.error("[POST] GET post by id: ", err);
+            return callback({ status: 404 });
+        } else {
+            console.log("[POST] GET post by id: ", result);
+            post = result[0];
+
+            const updateSql = `UPDATE Posts SET hits = hits + 1 WHERE id = ?`;
+            conn.query(updateSql, [post_id], function (err, result) {
+                if (err) {
+                    console.error("[POST] UPDATE post hits: ", err);
+                    return callback({ status: 401 });
+                } else {
+                    console.log("[POST] UPDATE post hits: ", result);
+                    return callback({ status: 200, post: post });
+                }
+            });
+        }
+    });
+};
+
+const createPost = ({ user_id, title, content, image }, callback) => {
+    console.log("============ [POST] CREATE post");
+
+    if (!conn) {
+        console.error("[POST] CREATE post: DB connection error");
+        return callback({ status: 500 });
+    }
 
     if (!title || !content || !user_id) {
-        return { status: 400 };
+        return callback({ status: 400 });
     }
 
-    const lastPostId = postsJSON.length > 0 ? postsJSON[postsJSON.length - 1].post_id : 0;
+    const sql = `INSERT INTO Posts (title, content, image, userId, createdAt)
+                VALUES (?, ?, ?, ?, now())`;
 
-    const newPost = {
-        post_id: lastPostId + 1,
-        title: title,
-        content: content,
-        image: image,
-        user_id: parseInt(user_id),
-        created_at: getKoreanDateTime(),
-        updated_at: null,
-        deleted_at: null,
-        likes: 0,
-        comments: 0,
-        hits: 0,
-        comments_list: [],
-    };
-
-    postsJSON.push(newPost);
-    savePosts();
-
-    console.log("[POST] CREATE post: ", newPost.post_id);
-    return { status: 200, post_id: newPost.post_id };
-};
-
-const updatePost = ({ user_id, post_id, title, content, image }) => {
-    postsJSON = JSON.parse(
-        fs.readFileSync(path.join(__dirname, "../data", "posts.json"), "utf-8")
-    );
-
-    const postToUpdate = postsJSON.find((post) => post.post_id === parseInt(post_id));
-    if (!postToUpdate) {
-        return { status: 404 };
-    } else if (postToUpdate.user_id !== parseInt(user_id)) {
-        return { status: 409 };
-    }
-
-    if (title) {
-        postToUpdate.title = title;
-    }
-    if (content) {
-        postToUpdate.content = content;
-    }
-    if (image) {
-        postToUpdate.image = image;
-    }
-
-    postToUpdate.updated_at = getKoreanDateTime();
-    savePosts();
-
-    return { status: 200, post_updated_id: postToUpdate.post_id };
-};
-
-const deletePost = ({ user_id, post_id }) => {
-    postsJSON = JSON.parse(
-        fs.readFileSync(path.join(__dirname, "../data", "posts.json"), "utf-8")
-    );
-
-    const postIndex = postsJSON.findIndex((post) => post.post_id === parseInt(post_id));
-
-    if (postIndex === -1) {
-        return { status: 404 };
-    } else if (postsJSON[postIndex].user_id !== parseInt(user_id)) {
-        return { status: 409 };
-    }
-
-    postsJSON.splice(postIndex, 1);
-    savePosts();
-
-    return { status: 200, post_deleted_id: post_id };
-};
-
-// ===== COMMENTS =====
-
-const createComment = ({ user_id, post_id, comment }) => {
-    postsJSON = JSON.parse(
-        fs.readFileSync(path.join(__dirname, "../data", "posts.json"), "utf-8")
-    );
-
-    const postToUpdate = postsJSON.find((post) => post.post_id === parseInt(post_id));
-    if (!postToUpdate) {
-        return { status: 400 };
-    }
-
-    const lastCommentId =
-        postToUpdate.comments_list.length > 0
-            ? postToUpdate.comments_list[postToUpdate.comments_list.length - 1].comment_id
-            : 0;
-
-    const newComment = {
-        comment_id: lastCommentId + 1,
-        content: comment.content,
-        user_id: parseInt(user_id),
-        created_at: getKoreanDateTime(),
-        updated_at: null,
-    };
-
-    postToUpdate.comments_list.push(newComment);
-    postToUpdate.comments += 1;
-    savePosts();
-
-    console.log("[POST] CREATE comment: ", newComment.comment_id);
-    return { status: 200, comment_id: newComment.comment_id };
-};
-
-const updateComment = ({ user_id, post_id, comment_id, update_form }) => {
-    postsJSON = JSON.parse(
-        fs.readFileSync(path.join(__dirname, "../data", "posts.json"), "utf-8")
-    );
-
-    const postToUpdate = postsJSON.find((post) => post.post_id === parseInt(post_id));
-    if (!postToUpdate) {
-        return { status: 400 };
-    }
-
-    const commentToUpdate = postToUpdate.comments_list.find(
-        (comment) => comment.comment_id === parseInt(comment_id)
-    );
-    if (!commentToUpdate) {
-        return { status: 404 };
-    } else if (commentToUpdate.user_id !== parseInt(user_id)) {
-        return { status: 409 };
-    }
-
-    Object.keys(update_form).forEach((key) => {
-        commentToUpdate[key] = update_form[key];
+    conn.query(sql, [title, content, image, parseInt(user_id)], function (err, result) {
+        if (err) {
+            console.error("[POST] CREATE post: ", err);
+            return callback({ status: 400 });
+        } else {
+            console.log("[POST] CREATE post: ", result);
+            return callback({ status: 200, post_id: result.insertId });
+        }
     });
-
-    commentToUpdate.updated_at = getKoreanDateTime();
-    savePosts();
-
-    console.log("[POST] UPDATE comment: ", commentToUpdate.comment_id);
-    return { status: 200, comment_updated_id: commentToUpdate.comment_id };
 };
 
-const deleteComment = ({ user_id, post_id, comment_id }) => {
-    postsJSON = JSON.parse(
-        fs.readFileSync(path.join(__dirname, "../data", "posts.json"), "utf-8")
-    );
+const updatePost = ({ user_id, post_id, title, content, image }, callback) => {
+    console.log("============ [POST] UPDATE post");
 
-    const postToUpdate = postsJSON.find((post) => post.post_id === parseInt(post_id));
-    if (!postToUpdate) {
-        return { status: 400 };
+    if (!conn) {
+        console.error("[POST] UPDATE post: DB connection error");
+        return { status: 500 };
     }
 
-    const commentToDelete = postToUpdate.comments_list.find(
-        (comment) => comment.comment_id === parseInt(comment_id)
-    );
+    let sql = `UPDATE Posts SET `;
+    const updates = [];
+    const params = [];
 
-    if (!commentToDelete) {
-        return { status: 404 };
-    } else if (commentToDelete.user_id !== parseInt(user_id)) {
-        return { status: 409 };
+    if (title !== undefined) {
+        updates.push("title = ?");
+        params.push(title);
+    }
+    if (content !== undefined) {
+        updates.push("content = ?");
+        params.push(content);
+    }
+    if (image !== undefined) {
+        updates.push("image = ?");
+        params.push(image.path);
     }
 
-    postToUpdate.comments_list = postToUpdate.comments_list.filter(
-        (comment) => comment.comment_id !== parseInt(comment_id)
-    );
-    postToUpdate.comments -= 1;
-    savePosts();
+    sql += updates.join(", ");
+    sql += `, updatedAt = now() 
+            WHERE id = ?`;
+    params.push(post_id);
 
-    return { status: 200, comment_deleted_id: comment_id };
+    conn.query(sql, params, function (err, result) {
+        if (err) {
+            console.error("[POST] UPDATE post: ", err);
+            return callback({ status: 400 });
+        } else {
+            console.log("[POST] UPDATE post: ", result);
+            return callback({ status: 200, post_updated_id: post_id });
+        }
+    });
 };
 
-// ===== COMMON FUNCTIONS =====
+const deletePost = ({ user_id, post_id }, callback) => {
+    console.log("============ [POST] DELETE post");
 
-function savePosts() {
-    fs.writeFileSync(
-        path.join(__dirname, "../data", "posts.json"),
-        JSON.stringify(postsJSON)
-    );
-}
+    if (!conn) {
+        console.error("[POST] DELETE post: DB connection error");
+        return { status: 500 };
+    }
+
+    const sql = `DELETE FROM Posts WHERE id = ? AND userId = ?`;
+
+    conn.query(sql, [post_id, user_id], function (err, result) {
+        if (err) {
+            console.error("[POST] DELETE post: ", err);
+            return callback({ status: 400 });
+        } else {
+            console.log("[POST] DELETE post: ", result);
+            return callback({ status: 200, post_deleted_id: post_id });
+        }
+    });
+};
 
 // ===== EXPORT =====
 
@@ -272,7 +173,4 @@ module.exports = {
     createPost,
     updatePost,
     deletePost,
-    createComment,
-    updateComment,
-    deleteComment,
 };
